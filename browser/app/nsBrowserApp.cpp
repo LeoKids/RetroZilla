64f32473548fe9d38e7cae9ee38b2a7addc1a7a3
ClassicNick <75703478+ClassicNick@users.noreply.github.com>
2021-02-26 16:12:13 -0400

    Update nsBrowserApp.cpp and splash.rc
    This should fix issue #31 Winternight.exe not functional when rzbrowser.exe is running

--- browser/app/nsBrowserApp.cpp
+++ browser/app/nsBrowserApp.cpp
@@ -46,8 +46,8 @@
 static const nsXREAppData kAppData = {
   sizeof(nsXREAppData),
   nsnull,
-  "RetroZilla",
-  "Browser",
+  "Winternight",
+  "Classic",
   NS_STRINGIFY(APP_VERSION),
   NS_STRINGIFY(BUILD_ID),
   "{ec8030f7-c20a-464f-9b0e-13a3a9e97384}",
